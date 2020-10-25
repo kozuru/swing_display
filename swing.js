6aa89d7450070a1433d20631eb79cba182a908b7
kozuru <53803527+kozuru@users.noreply.github.com>
2020-10-26 08:32:30 +0900

    Update swing.js

--- swing.js
+++ swing.js
@@ -9,7 +9,8 @@ $('document').ready(function () {
     $('#google_translate_element').on("click", function () {
 
         // Change font family and color
-        $("iframe").contents().find(".goog-te-menu2-item div, .goog-te-menu2-item:link div, .goog-te-menu2-item:visited div, .goog-te-menu2-item:active div, .goog-te-menu2 *")
+        //$("iframe").contents().find(".goog-te-menu2-item div, .goog-te-menu2-item:link div, .goog-te-menu2-item:visited div, .goog-te-menu2-item:active div, .goog-te-menu2 *")
+         $("iframe").contents().find(".goog-te-menu2-item div,  .goog-te-menu2-item:visited div, .goog-te-menu2-item:active div, .goog-te-menu2 *")    
             .css({
                 'color': '#544F4B',
                 'font-family': 'Roboto',
